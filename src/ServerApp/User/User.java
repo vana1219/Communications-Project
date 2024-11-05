21b96812ec6ea5e5fe8b6ed05a5ee10606adcfc3
Ayoub Mekkaoui <89435167+Ayoub-Mekkaoui@users.noreply.github.com>
2024-11-05 15:26:52 -0800

    Update User.java
    Updated User.java with all of the attributes and methods found in our class diagram as of 11/5

---
+++ src/ServerApp/User/User.java
@@ -0,0 +1,68 @@
+package src.ServerApp.User;
+
+public class User {
+    private String userID;
+    private String username;
+    private String password;
+    private boolean isOnline;
+    private boolean isBanned;
+
+    // Constructor
+    public User(String userID, String username, String password) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.isOnline = false;
+        this.isBanned = false;
+    }
+
+    // Getters and setters
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean isOnline) {
+        this.isOnline = isOnline;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean isBanned) {
+        this.isBanned = isBanned;
+    }
+
+    // Methods from the class diagram
+    public boolean checkIfBanned() {
+        return isBanned;
+    }
+
+    public boolean checkIfOnline() {
+        return isOnline;
+    }
+}
