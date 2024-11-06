ce86d7ee8e8a7dc6682ada30d783d7ea5fa14ae9
Ayoub Mekkaoui <89435167+Ayoub-Mekkaoui@users.noreply.github.com>
2024-11-05 16:08:25 -0800

    Created Admin class
    Created Admin class, only attributes and method outline
    
    NOTE: Methods need to be coded, only placeholders are set for now

---
+++ src/ServerApp/Admin/Admin.java
@@ -0,0 +1,65 @@
+package src.ServerApp.Admin;
+import src.ServerApp.User.User; 
+
+// Admin INHERENTS from User
+public class Admin extends User {
+
+    // Constructor
+    public Admin(String userID, String username, String password) {
+        super(userID, username, password);
+    }
+
+    // Methods 
+    // NOTE - METHOD NOT COMPLETE
+    public boolean banUser(String userID) {
+    	// Placeholder for method implementation
+        return false;
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public boolean unbanUser(String userID) {
+        // Placeholder for method implementation
+        return false;
+    }
+    
+    // NOTE - METHOD NOT COMPLETE
+    public boolean addUser(User user) {
+        // Placeholder for method implementation
+        return false;
+    }
+    
+    // NOTE - METHOD NOT COMPLETE
+    public boolean deleteUser(String userID) {
+        // Placeholder for method implementation
+        return false;
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public boolean resetUserPassword(String userID, String newPassword) {
+        // Placeholder for method implementation
+        return false;
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public void sendSystemMessage(String content) {
+        // Placeholder for method implementation
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public boolean hideChatMessage(String chatBoxID, String messageID) {
+        // Placeholder for method implementation
+        return false;
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public boolean hideChatBox(String chatID) {
+        // Placeholder for method implementation
+        return false;
+    }
+
+    // NOTE - METHOD NOT COMPLETE
+    public String getChatBoxLog(String chatBoxID) {
+        // Placeholder for method implementation
+        return "";
+    }
+}
