f747bdefe7f14ee8609e87f13bd3cabf77a0d330
Ibraheem Fawal <ibraheemfawal@gmail.com>
2024-11-05 18:58:35 -0800

    Added Client skeleton

--- src/ClientApp/Client/Client.java
+++ src/ClientApp/Client/Client.java
@@ -1,8 +1,59 @@
 package Client;
 
+import java.net.Socket;
+import java.util.Map;
+import java.util.Queue;
+
 public class Client {
+	private String serverAddress;
+	private DataInputStream inputStream;
+	private Socket socket;
+	private int port;
+	private Queue messageQueue;
+	private User userData;
+	private Map ChatBoxList;
+	
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
     }
+}
+
+public DataInputStream ClientMessageThread() {
+
+}
+
+public void sendMessageRequest() {
+	
+}
+
+public void sendChatBoxRequest() {
+	
+}
+
+public void sendUnbanUserRequest() {
+	
+}
+
+public void sendResetUserPassword() {
+	
+}
+
+public void sendCreateUserRequest() {
+	
+}
+
+public String receiveResponse() {
+	
+}
+
+public void receiveChatBoxStatus() {
+	
+}
+
+public void sendViewChatBoxLogRequest() {
+	
+}
+
+public void receiveChatBoxLog() {
+	
 }
