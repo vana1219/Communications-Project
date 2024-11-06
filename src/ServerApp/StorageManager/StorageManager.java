7a323f9eb2f1c4bd86b662b271a620e45fe5cf11
Ayoub Mekkaoui <89435167+Ayoub-Mekkaoui@users.noreply.github.com>
2024-11-05 17:22:02 -0800

    Created StorageManager Class
    added the hashmap attribute and methods to load and save chatbox objects from file
    
    NOTE: there is 2 additional helper methods (update SRS doc)

---
+++ src/ServerApp/StorageManager/StorageManager.java
@@ -0,0 +1,46 @@
+package src.ServerApp.StorageManager;
+
+import src.ServerApp.ChatBox.ChatBox;
+import java.util.HashMap;
+import java.io.*;
+
+public class StorageManager {
+
+    // Attributes
+    private HashMap<Integer, ChatBox> chatBoxRecords;
+
+    // Constructor
+    public StorageManager() {
+        this.chatBoxRecords = new HashMap<>();
+        loadChatBoxesFromFile(); // Load existing chatboxes from file
+    }
+
+    // Methods
+    public boolean storeChatBox(ChatBox chatBox) {
+        chatBoxRecords.put(chatBox.getChatBoxID(), chatBox);
+        saveChatBoxesToFile(); // Save updated chatboxes to file
+        return true;
+    }
+
+    public ChatBox retrieveChatBox(int chatID) {
+        loadChatBoxesFromFile(); // Load chatboxes from file before retrieving
+        return chatBoxRecords.get(chatID);
+    }
+
+    private void saveChatBoxesToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("chatBoxes.ser"))) {
+            oos.writeObject(chatBoxRecords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadChatBoxesFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("chatBoxes.ser"))) {
+            chatBoxRecords = (HashMap<Integer, ChatBox>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            chatBoxRecords = new HashMap<>(); // If file not found or error occurs, start with an empty HashMap
+        }
+    }
+}
+
