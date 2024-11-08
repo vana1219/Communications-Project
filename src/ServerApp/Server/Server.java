a9adbb304e7f77eb74191a0790307b353e6a062a
vana1219 <71721931+vana1219@users.noreply.github.com>
2024-11-07 21:40:31 -0800

    Update Server and ClientHandler

--- src/ServerApp/Server/Server.java
+++ src/ServerApp/Server/Server.java
@@ -1,9 +1,89 @@
 package ServerApp.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+
+import ServerApp.ClientHandler.ClientHandler;
+import ServerApp.User.User;
+
+
 public class Server {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	
+	private String ipAddress;
+	private List<ClientHandler> clientHandler;
+	private MessageHandler messageHandler;
+	private HashMap<String, User> userDB;
+	private static ServerSocket serverSocket=null;
+	private int port;
+	
+	
+	public static void main(String[] args) {
+		int port=5627;
+		
+		Server server = new Server();
+		server.setPort(port);
+		try{
+			server.startServer(port);
+		 
+			while(true) {
+				try {
+					server.acceptClient();
+					}
+				catch (IOException e) {
+                    System.err.println("Error accepting client connection: " + e.getMessage());
+                }	
+			 }
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			server.shutDownServer();
+			}
+		}
 
-    }
-}
+	public void setPort(int port) {
+		this.port=port;
+	}
+	public int getPort() {
+		return this.port;
+	}
+	public void startServer(int port) throws IOException{
+		serverSocket = new ServerSocket(port);		//creates a new ServerSocket object that listens for incoming connections
+		System.out.println("The server is running in port: "+ port);
+		serverSocket.setReuseAddress(true);
+		
+	}
+	public void acceptClient() throws IOException{
+		try {
+			Socket client=serverSocket.accept();
+			 
+			// Displaying that new client is connected to server
+			System.out.println("New client connected " + client);
+			
+			// create a new thread object
+			ClientHandler clientSock = new ClientHandler(client);
 
+			// This thread will handle the client separately
+			new Thread(clientSock).start();
+			}
+		catch (IOException e) {
+            System.err.println("Error accepting client connection: " + e.getMessage());
+        }	
+	}
+	public void shutDownServer() {
+		if (serverSocket != null && !serverSocket.isClosed()) {
+            try {
+                serverSocket.close();
+                System.out.println("Server shut down successfully.");
+            } catch (IOException e) {
+                System.err.println("Error shutting down server: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+	}
+  
+	}
