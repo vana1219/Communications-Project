13926c8b62b09fe81ed2175675709c0c5c858998
catcallofcthulhu <69273223+catcallofcthulhu@users.noreply.github.com>
2024-12-01 18:50:22 -0800

    finished handleReturnUserList

--- src/ClientApp/Client/Client.java
+++ src/ClientApp/Client/Client.java
@@ -92,6 +92,7 @@ public class Client {
     // Handle SendUserList messages
     private void handleReturnUserList(SendUserList sendUserList) {
         List<User> userList = sendUserList.userList();
+        gui.updateUserList(userList);
         // Process user list as needed
     }
 
