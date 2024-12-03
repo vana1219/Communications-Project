31ba4a7398b3e20abe91d5223f4457830b78432a
Ibraheem Fawal <ifawal@horizon.csueastbay.edu>
2024-12-03 03:29:12 -0800

    Update TestMessage
    added comments for clarity of the test functions

--- src/Testing/TestMessage.java
+++ src/Testing/TestMessage.java
@@ -13,12 +13,16 @@ class TestMessage {
 	private int senderID;
 	private String content;
 
+	
+	// Create Message object to test, id = 1
 	@BeforeEach
 	public void setUpMessage() {
 		message= new Message(1,"This is a New Message");
 	}
+	
 	@Test
 	void testMessageConstructorSenderIDandtext() {
+		// Make sure the object isn't null
 		assertNotNull(message);
 		assertEquals(1, message.getSenderID());
 		assertEquals("This is a New Message", message.getContent());
@@ -31,16 +35,22 @@ class TestMessage {
 		assertEquals(1, newMessage.getSenderID());
 		assertEquals("This is a New Message",newMessage.getContent());
 	}
+	
+	// Check that the senders ID is correct
 	@Test
 	void testGetSenderID() {
 		assertEquals(1, message.getSenderID());
 	}
+	
+	// Get the Message id and check 
 	@Test
 	void testGetMessageID() {
 		Message newMessage=new Message(message);	
 		System.out.println(message.getMessageID());
 		assertEquals(4, message.getMessageID());
 	}
+	
+	// Validate the contents of the message
 	@Test
 	void testGetConTent() {
 		assertEquals("This is a New Message",message.getContent());
